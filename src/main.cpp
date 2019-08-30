8c3cad9218e48299f04d644dd53eb3aac14de918
imdigo <limdongyoung0@gmail.com>
2019-08-30 02:35:54 +0000

    multiline cout

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!!" << endl;
+	// cout << "Hello, World!!" << endl;
+	std::cout << "hi" << std::endl
+          << "my name is "
+          << "Psi" << std::endl;
     return 0;
 }
