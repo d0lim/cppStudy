778ab81a0b23fc108c22c8a9aa609b3734265c9b
Dongyoung Lim <limdongyoung0@gmail.com>
2019-09-07 04:17:04 +0000

    Learned about class, how to make object with 2 ways, constructor, destructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,103 @@
 #include <iostream>
 
-
-class Animal {
-	private:
-	int food;
-	int weight;
+class Marine {
+	int hp;
+	int coordX, coordY;
+	int damage;
+	bool isDead;
+public:
+	Marine();
+	Marine(int x, int y);
+	~Marine();
 	
-	public:
-	void setAnimal(int _food, int _weight) {
-		food = _food;
-		weight = _weight;
-	}
-	void increaseFood(int feed) {
-		food += feed;
-		weight += (feed / 3);
-	}
-	void viewStat() {
-		std::cout << "This animal has " << food << " food" << std::endl;
-		std::cout << "This animal's weight is " << weight <<  std::endl;
-	}
-}; // Do not forget semicolon !!!
-
-int main() {
-	Animal animal;
-	animal.setAnimal(100, 50);
-	animal.increaseFood(30);
+	int attack();
+	void beAttacked(int attackedDamage);
+	void move(int x, int y);
 	
-	animal.viewStat();
-	return 0;
+	void showStatus();
+};
+
+
+Marine::Marine() {
+	hp = 50;
+	coordX = coordY = 0;
+	damage = 5;
+	isDead = false;
+}
+Marine::Marine(int x, int y) {
+	hp = 50;
+	coordX = x;
+	coordY = y;
+	damage = 5;
+	isDead = false;
+}
+Marine::~Marine() {
+	std::cout << "Marine Deleted!" << std::endl;
 }
 
+void Marine::move(int x, int y) {
+	coordX = x;
+	coordY = y;
+}
+int Marine::attack() {
+	return damage;
+}
+void Marine::beAttacked(int attackedDamage) {
+	if (!isDead) {
+		hp -= attackedDamage;
+		if (hp <= 0) {
+			isDead = true;
+			hp = 0;
+		}
+	} else {
+		std::cout << "Already Dead" << std::endl;
+	}
+}
+void Marine::showStatus() {
+	std::cout << "====================================================" << std::endl;
+	std::cout << "HP : " << hp << std::endl;
+	std::cout << "Coordinate : (" << coordX << ", " << coordY << ")" << std::endl;
+	std::cout << "Damage : " << damage << std::endl;
+	std::cout << "====================================================" << std::endl;
+}
+
+void test1() {
+	/* This is first method to make object. 
+	   Dynamic allocation.
+	   Save object to heap of memory. */
+	Marine* marine1 = new Marine(1, 1);
+	Marine* marine2 = new Marine(3, 3);
+	
+	marine1->showStatus();
+	marine2->showStatus();
+	
+	marine1->move(0, 0);
+	marine1->beAttacked(marine2->attack());
+	
+	marine1->showStatus();
+	marine2->showStatus();
+}
+
+void test2() {
+	/* This is second method to make object. 
+	   Static allocation.
+	   Save object to stack of memory.
+	   If function ends, objects will be deleted. */
+	Marine marine1(1, 1);
+	Marine marine2(3, 3);
+	
+	marine1.showStatus();
+	marine2.showStatus();
+	
+	marine1.move(0, 0);
+	marine1.beAttacked(marine2.attack());
+	
+	marine1.showStatus();
+	marine2.showStatus();
+}
+
+int main() {
+	test1();
+	// test2();
+	return 0;
+}
