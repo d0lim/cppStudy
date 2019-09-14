628119ba880fbb293c4e07db2057c819aeeb897f
Dongyoung Lim <limdongyoung0@gmail.com>
2019-09-14 06:57:36 +0000

    Learned about virtual keyword and inheritance

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,98 @@
 #include <iostream>
+#include <string>
 
-class Complex {
-	double real, img;
-	
-public:
-	Complex(double real, double img);
-	Complex operator+(const Complex &c);
-	Complex operator-(const Complex &c);
-	Complex operator*(const Complex &c);
-	Complex operator/(const Complex &c);
-	
-	Complex operator+=(const Complex &c);
-	Complex operator-=(const Complex &c);
-	Complex operator*=(const Complex &c);
-	Complex operator/=(const Complex &c);
-	
-	Complex& operator=(const Complex &c);
-	void println();
+class Employee {
+ protected:
+  std::string name;
+  int age;
+
+  std::string position;  // 직책 (이름)
+  int rank;              // 순위 (값이 클 수록 높은 순위)
+
+ public:
+  Employee(std::string name, int age, std::string position, int rank)
+      : name(name), age(age), position(position), rank(rank) {}
+
+  // 복사 생성자
+  Employee(const Employee& employee) {
+    name = employee.name;
+    age = employee.age;
+    position = employee.position;
+    rank = employee.rank;
+  }
+
+  // 디폴트 생성자
+  Employee() {}
+
+  virtual void print_info() {
+    std::cout << name << " (" << position << " , " << age << ") ==> "
+              << calculate_pay() << "만원" << std::endl;
+  }
+  virtual int calculate_pay() { return 200 + rank * 50; }
 };
 
-Complex::Complex(double real, double img) : real(real), img(img) {}
+class Manager : public Employee {
+  int year_of_service;
 
-Complex Complex::operator+(const Complex &c) {
-	Complex temp(real + c.real, img + c.img);
-	return temp;
-}
-Complex Complex::operator-(const Complex &c) {
-	Complex temp(real - c.real, img + c.img);
-	return temp;
-}
-Complex Complex::operator*(const Complex &c) {
-	Complex temp(real * c.real - img * c.img, real * c.img + img * c.real);
-	return temp;
-}
-Complex Complex::operator/(const Complex &c) {
-	Complex temp((real * c.real + img * c.img) / (c.real * c.real + c.img * c.img),
-    (img * c.real - real * c.img) / (c.real * c.real + c.img * c.img));
-	return temp;
-}
+ public:
+  Manager(std::string name, int age, std::string position, int rank,
+          int year_of_service)
+      : year_of_service(year_of_service), Employee(name, age, position, rank) {}
 
-Complex Complex::operator+=(const Complex &c){
-	(*this) = (*this) + c;
-	return *this;
-}
-Complex Complex::operator-=(const Complex &c){
-	(*this) = (*this) - c;
-	return (*this);
-}
-Complex Complex::operator*=(const Complex &c){
-	(*this) = (*this) * c;
-	return (*this);
-}
-Complex Complex::operator/=(const Complex &c){
-	(*this) = (*this) / c;
-	return (*this);
-}
+  int calculate_pay() override { return 200 + rank * 50 + 5 * year_of_service; }
+  void print_info() override {
+    std::cout << name << " (" << position << " , " << age << ", "
+              << year_of_service << "년차) ==> " << calculate_pay() << "만원"
+              << std::endl;
+  }
+};
+class EmployeeList {
+  int alloc_employee;        // 할당한 총 직원 수
+  int current_employee;      // 현재 직원 수
+  Employee** employee_list;  // 직원 데이터
 
-Complex& Complex::operator=(const Complex &c) {
-	real = c.real;
-	img = c.img;
-	return *this;
-}
+ public:
+  EmployeeList(int alloc_employee) : alloc_employee(alloc_employee) {
+    employee_list = new Employee*[alloc_employee];
+    current_employee = 0;
+  }
+  void add_employee(Employee* employee) {
+    // 사실 current_employee 보다 alloc_employee 가 더
+    // 많아지는 경우 반드시 재할당을 해야 하지만, 여기서는
+    // 최대한 단순하게 생각해서 alloc_employee 는
+    // 언제나 current_employee 보다 크다고 생각한다.
+    // (즉 할당된 크기는 현재 총 직원수 보다 많음)
+    employee_list[current_employee] = employee;
+    current_employee++;
+  }
+  int current_employee_num() { return current_employee; }
 
+  void print_employee_info() {
+    int total_pay = 0;
+    for (int i = 0; i < current_employee; i++) {
+      employee_list[i]->print_info();
+      total_pay += employee_list[i]->calculate_pay();
+    }
 
-void Complex::println() {
-	if (img > 0)
-		std::cout << real << "+" << img << "i" << std::endl;
-	else if (img == 0)
-		std::cout << real <<std::endl;
-	else
-		std::cout << real << img << "i" << std::endl;
-}
-
+    std::cout << "총 비용 : " << total_pay << "만원 " << std::endl;
+  }
+  ~EmployeeList() {
+    for (int i = 0; i < current_employee; i++) {
+      delete employee_list[i];
+    }
+    delete[] employee_list;
+  }
+};
 int main() {
-	Complex *p1 = new Complex(1, 1);
-	Complex *p2 = new Complex(4, -2);
-	Complex *p3 = new Complex(4, 2);
-	p1->println();
-	p2->println();
-	*p3 += *p1 + *p2 + *p1;
-	p3->println();
-	
-	return 0;
+  EmployeeList emp_list(10);
+  emp_list.add_employee(new Employee("노홍철", 34, "평사원", 1));
+  emp_list.add_employee(new Employee("하하", 34, "평사원", 1));
+
+  emp_list.add_employee(new Manager("유재석", 41, "부장", 7, 12));
+  emp_list.add_employee(new Manager("정준하", 43, "과장", 4, 15));
+  emp_list.add_employee(new Manager("박명수", 43, "차장", 5, 13));
+  emp_list.add_employee(new Employee("정형돈", 36, "대리", 2));
+  emp_list.add_employee(new Employee("길", 36, "인턴", -2));
+  emp_list.print_employee_info();
+  return 0;
 }
